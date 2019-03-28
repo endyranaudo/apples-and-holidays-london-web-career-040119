8c0dda1aade473f117314d2cc3b4364c833f62ad
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-28 13:49:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,9 +52,11 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  new_arr = []
   holiday_hash[:winter].map do |holiday, supplies|
-    binding.pry
+    new_arr << supplies
   end
+  new_arr.flatten
 end
 
 
