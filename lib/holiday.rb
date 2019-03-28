3649c6fbe6b489a4187eaf521066a5db6fc57f10
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-28 14:13:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect do |season, holiday|
-    if holiday.include
+    if holiday.include?
   end
 end
 
